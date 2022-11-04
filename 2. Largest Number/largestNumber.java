8bac700592f954a84d9a652fcc346472c808c496
SAHIL ALI <84962462+SahilAli8808@users.noreply.github.com>
2022-11-05 00:26:13 +0530

    Update largestNumber.java

--- 2. Largest Number/largestNumber.java
+++ 2. Largest Number/largestNumber.java
@@ -1,3 +1,26 @@
+//program to find the largest number among three numbers
+import java.util.Scanner;
 public class largestNumber {
-    
+    public static void main(String[] args) {
+        int num1, num2, num3;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        num1 = s.nextInt();
+        System.out.print("Enter the second number:");
+        num2 = s.nextInt();
+        System.out.print("Enter the third number:");
+        num3 = s.nextInt();
+        if(num1 > num2 && num1 > num3)
+        {
+            System.out.println("Largest number:"+num1);
+        }
+        else if(num2 > num1 && num2 > num3)
+        {
+            System.out.println("Largest number:"+num2);
+        }
+        else
+        {
+            System.out.println("Largest number:"+num3);
+        }
+    }
 }
